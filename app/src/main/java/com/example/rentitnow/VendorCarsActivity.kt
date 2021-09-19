c8b3f3d086d5882c00e90b055dc9871cc4fa1b5a
bomma <abhishekreddybomma@gmail.com>
2021-09-18 21:23:56 -0700

    Vendor UI n Nav Bar

---
+++ app/src/main/java/com/example/rentitnow/VendorCarsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.rentitnow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VendorCarsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vendor_cars)
+    }
+}
