69fb8722dfdeb07a267aec6734fe83056b868cf9
bomma <abhishekreddybomma@gmail.com>
2021-09-21 18:24:28 -0700

    Layout

--- app/src/main/java/com/example/rentitnow/LoginActivity.kt
+++ app/src/main/java/com/example/rentitnow/LoginActivity.kt
@@ -4,9 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.facebook.CallbackManager
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var callbackManager: CallbackManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
