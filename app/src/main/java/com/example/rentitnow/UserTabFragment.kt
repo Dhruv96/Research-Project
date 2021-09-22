46f3f7167ea7373234951e90086560e817618527
Dhruv Grover <dhruvgrover111@gmail.com>
2021-09-21 18:11:56 -0700

    Google Sign In Implemented

--- app/src/main/java/com/example/rentitnow/UserTabFragment.kt
+++ app/src/main/java/com/example/rentitnow/UserTabFragment.kt
@@ -1,6 +1,5 @@
 package com.example.rentitnow
 
-import android.app.Activity
 import android.content.ContentValues.TAG
 import android.content.Intent
 import android.net.Uri
@@ -11,7 +10,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.RadioGroup
 import android.widget.Toast
-import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
