5db46f6a0a6fa84933b3f93a9a8e9542d79b93b7
Dhruv Grover <dhruvgrover111@gmail.com>
2021-10-10 00:51:20 -0700

    Published Vehicles Screen Implemented

--- app/src/main/java/com/example/rentitnow/Vehicle.kt
+++ app/src/main/java/com/example/rentitnow/Vehicle.kt
@@ -6,12 +6,43 @@ enum class VehicleType (val type: String){
     TRUCK("truck")
 }
 
-class Vehicle (
-        var type: String,
-        var costPerDay: Float,
-        var imageUrls: List<String>,
-        var color: String,
-        var vendorID: String,
-        var model: String,
-        var manufacture: String
-        )
+enum class FuelType (val type: String){
+    PETROL("Petrol"),
+    DIESEL("Diesel")
+}
+
+enum class TransmissionType (val type: String){
+    AUTOMATIC("Automatic"),
+    MANUAL("Manual")
+}
+
+class Vehicle {
+    var type: String = ""
+    var costPerDay: Float = 0.0f
+    var imageUrls: List<String> = listOf()
+    var vendorID: String = ""
+    var model: String = ""
+    var manufacture: String = ""
+    var transmissionType: String = ""
+    var description: String = ""
+    var fuelType: String = ""
+
+
+    constructor(type: String, costPerDay: Float, imageUrls: List<String>, vendorID: String, model: String, manufacture: String, transmissionType: String, description: String, fuelType: String) {
+        this.type = type
+        this.costPerDay = costPerDay
+        this.imageUrls = imageUrls
+        this.vendorID = vendorID
+        this.model = model
+        this.manufacture = manufacture
+        this.transmissionType = transmissionType
+        this.description = description
+        this.fuelType = fuelType
+    }
+
+    constructor() {
+
+    }
+
+
+}
